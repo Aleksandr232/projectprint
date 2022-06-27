9f9249f14b7dfc6dde6b1018ee6f25862745164b
Aleksandr232 <aleksrowing232@gmail.com>
2022-06-27 19:44:40 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React,{Component} from 'react';
-import Printer, { print } from 'react-pdf-print'
+import Printer, { print } from 'react-pdf-print';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
+
 
 const ids = ['1']
 
 class App extends Component{
   render() {
     return (
-      <div className='App'>
+      <div className>
         <Printer>
             <div id={ids[0]} style={{ width:'210mm', height: '297mm' }}>
                 Hello World!
             </div>
         </Printer>
-        <input type='button' style={{ position: 'relative', float: 'right' }}
-          onClick={() => print(ids)} value='Stampa' />
+        
+          <button
+          className='btn btn-info ms-2'
+          onClick={() => print(ids)} value='Stampa'
+          >Печать</button>
       </div>
     )
   }
